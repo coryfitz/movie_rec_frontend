1511c14dddc516dbea81c451b6886bea38cddc33
Cory Fitz <coryalanfitz@gmail.com>
2023-09-24 12:45:27 +0100

    organized routes into index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import './Axios';
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Logout from "./Logout";
+import Signup from "./Signup";
+import UserRecommender from "./UserRecommender";
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/logout" element={<Logout/>} />
+            <Route path="/signup" element={<Signup/>} />
+            <Route path="/userrecommender" element={<UserRecommender/>} />
+          </Routes>
+        </BrowserRouter>  
   </React.StrictMode>
 );
